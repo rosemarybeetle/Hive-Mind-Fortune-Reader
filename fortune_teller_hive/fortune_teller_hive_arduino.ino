9f7a6fbed84496093a33136934d213fd4bc03cad
Rosemary Beetle <rosemarybeetle@gmail.com>
2013-03-29 22:55:06 +0000

    Minor tidy ups.

--- fortune_teller_hive/fortune_teller_hive_arduino.ino
+++ fortune_teller_hive/fortune_teller_hive_arduino.ino
@@ -1,14 +1,12 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  Fortune Teller Hive Arduino board code
+  Needed to control physical interaction...
+  and relay physical-detection data back to Processing mothership!
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+
 int switchPin = A0; // Analogue in = A0, called switchSensor.
-int led = 13;
+int led = 13; // LED pin...
 int analogValue = 0; // this is used to determine whether to make a call to Twitter (if high) 
 
 // the setup routine runs once when you press reset:
@@ -22,12 +20,11 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //delay(1000);// wait for a second
   analogValue = analogRead(switchPin);   
   if (analogValue >=900) {
   Serial.println("fireTwitterCall");
   analogValue = 0; // reset - this is used to ensure the value is reset after a successful release of the switch
+  // ADD ANY OTHER TRIGGERS HERE THAT COME FROM THIS INTERACTION
   }
  // digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   //delay(1000);               // wait for a second
