47b86a356657818cecf9af39829813cf1acd8885
Rosemary Beetle <rosemarybeetle@gmail.com>
2013-03-29 16:38:24 +0000

    Added support for physical button

--- fortune_teller_hive/fortune_teller_hive_arduino.ino
+++ fortune_teller_hive/fortune_teller_hive_arduino.ino
@@ -1,5 +1,5 @@
 /*
-  Based on the nursery file "Blink"
+  Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
  
   This example code is in the public domain.
@@ -7,7 +7,9 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
+int switchPin = A0; // Analogue in = A0, called switchSensor.
 int led = 13;
+int analogValue = 0; // this is used to determine whether to make a call to Twitter (if high) 
 
 // the setup routine runs once when you press reset:
 void setup() {   
@@ -20,9 +22,13 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);// wait for a second
-  Serial.println("are you receiving"); // this is the Hello World send over serial port
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  // digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //delay(1000);// wait for a second
+  analogValue = analogRead(switchPin);   
+  if (analogValue >=900) {
+  Serial.println("fireTwitterCall");
+  analogValue = 0; // reset - this is used to ensure the value is reset after a successful release of the switch
+  }
+ // digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  //delay(1000);               // wait for a second
 }
