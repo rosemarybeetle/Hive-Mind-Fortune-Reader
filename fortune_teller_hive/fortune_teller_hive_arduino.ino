40fe54f42000643e60aa09e59136c5a844826e47
Rosemary Beetle <rosemarybeetle@gmail.com>
2013-03-31 18:17:32 +0000

    Added in timer to prevent repeatedly sending data
    Writing to the port on a loop was confusing the Processign sketch.
    
    Despite efforts in Processing, I couldn't get the skecth stable, and it was send 2 tweets in quick succession.
    This fix stops the ARDUINO port sending anything for at least 5 seconds

--- fortune_teller_hive/fortune_teller_hive_arduino.ino
+++ fortune_teller_hive/fortune_teller_hive_arduino.ino
@@ -8,6 +8,9 @@
 int switchPin = A0; // Analogue in = A0, called switchSensor.
 int led = 13; // LED pin...
 int analogValue = 0; // this is used to determine whether to make a call to Twitter (if high) 
+float timerPeriod=5000; //don't send data more often than this 
+float timerSend=millis();
+float timerCheck=0;
 
 // the setup routine runs once when you press reset:
 void setup() {   
@@ -22,9 +25,13 @@ void setup() {
 void loop() {
   analogValue = analogRead(switchPin);   
   if (analogValue >=900) {
+    timerSend=millis();
+  if ((timerSend-timerCheck)>timerPeriod) {
   Serial.write("fireTweet");
+  timerCheck=millis();
   analogValue = 0; // reset - this is used to ensure the value is reset after a successful release of the switch
   // ADD ANY OTHER TRIGGERS HERE THAT COME FROM THIS INTERACTION
+  }
   }
  // digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   //delay(1000);               // wait for a second
